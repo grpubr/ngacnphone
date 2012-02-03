7c024b2f410992d949accbc299eb1987e0c75403
grpubr <grpubr@gmail.com>
2012-02-03 17:00:49 +0000

    convert [img] [url] tag to html tag add a fucntion to remove html tag "<br/>

--- nga_phone_base_3.0/src/sp/phone/utils/StringUtil.java
+++ nga_phone_base_3.0/src/sp/phone/utils/StringUtil.java
@@ -145,9 +145,19 @@ public class StringUtil {
 		// 转换 表情
 
 		s = s.replaceAll("(\\[s:\\d\\])", "<img src='$1'>");
+		//[url][/url]
+		s = s.replaceAll("\\[url\\](http\\S+)\\[/url\\]", "$1");
+		//[img]./111.jpg[/img]
+		s = s.replaceAll("\\[img\\]\\.(/\\S+[png|gif|jpg|bmp])\\[/img\\]", "<img src=\"http://img.ngacn.cc/attachments$1$1\">");
+		//[img]http://[/img]
+		s = s.replaceAll("\\[img\\](http\\S+[png|gif|jpg|bmp])\\[/img\\]", "<img src='$1'>");
 		return s;
 	}
 
+	public static String removeHtmlTag(String s){
+		s =s.replaceAll("<br/>", "\n");
+		return s;
+	}
 	/**
 	 * 处理URL
 	 * 
