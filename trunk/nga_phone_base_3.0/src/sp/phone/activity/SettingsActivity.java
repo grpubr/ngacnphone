28e9372aba98348bf6c3a6f6983fa0f8fca31655
grpubr@gmail.com <grpubr@gmail.com@56283f58-6903-3413-8642-665b0cdce46c>
2012-02-08 16:49:17 +0000

    夜间模式的设置代码

--- trunk/nga_phone_base_3.0/src/sp/phone/activity/SettingsActivity.java
+++ trunk/nga_phone_base_3.0/src/sp/phone/activity/SettingsActivity.java
@@ -1,28 +1,85 @@
 package sp.phone.activity;
 
+import sp.phone.utils.ThemeManager;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import android.widget.RadioGroup;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity{
 
+	private View view;
+	private CheckBox checkBoxDownimgNowifi;
+	private CheckBox nightMode;
+	//private MyGestureListener gestureListener;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.settings);
-		CheckBox checkBoxDownimgNowifi = (CheckBox) findViewById(R.id.checkBox_down_img_no_wifi);
+		//gestureListener = new MyGestureListener(this);
+		
+		initView();
+		
+	}
+	void initView(){
 		MyApp app = (MyApp) getApplication();
+		//checkbox
+		try{
+		view = getLayoutInflater().inflate(R.layout.settings, null);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		this.setContentView(view);
+		
+		checkBoxDownimgNowifi = (CheckBox) findViewById(R.id.checkBox_down_img_no_wifi);
 		checkBoxDownimgNowifi.setChecked(app.isDownImgWithoutWifi());
 		CheckBoxDownimgNowifiChangedListener listener = new CheckBoxDownimgNowifiChangedListener();
 		checkBoxDownimgNowifi.setOnCheckedChangeListener(listener);
+
+		nightMode = (CheckBox) findViewById(R.id.checkBox_night_mode);
+		nightMode.setChecked(ThemeManager.getInstance().getMode() ==ThemeManager.MODE_NIGHT);
+		nightMode.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+
+			@Override
+			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+				// TODO Auto-generated method stub
+				SharedPreferences  share = 
+					getSharedPreferences("perference", MODE_PRIVATE);
+
+				Editor editor = share.edit();
+				editor.putBoolean("nightmode", arg1);
+				editor.commit();
+				int mode = ThemeManager.MODE_NORMAL;
+				if(arg1)
+					mode =ThemeManager.MODE_NIGHT;
+				ThemeManager.getInstance().setMode(mode);
+				updateThemeUI();
+			}
+			
+		});
+	
+		
+		//TextView textv =(TextView)findViewById(R.id.fling_text);
+		//textv.setOnTouchListener(this.gestureListener);
+		updateThemeUI();
 	}
+	
+	private void updateThemeUI(){
+		checkBoxDownimgNowifi.setTextColor(
+				this.getResources().getColor(
+						ThemeManager.getInstance().getForegroundColor()));
+		nightMode.setTextColor(
+				this.getResources().getColor(
+						ThemeManager.getInstance().getForegroundColor()));
+		
+		view.setBackgroundResource(ThemeManager.getInstance().getBackgroundColor());
+	}
+	
 	class CheckBoxDownimgNowifiChangedListener implements OnCheckedChangeListener{
 
 		@Override
@@ -38,11 +95,7 @@ public class SettingsActivity extends Activity {
 			editor.commit();
 			
 		}
-
-		
-		
-		
 		
 	}
-
+	
 }
