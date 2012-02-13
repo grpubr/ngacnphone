d416785709d30f27dabfa55847e25561d1e90f85
grpubr@gmail.com <grpubr@gmail.com@56283f58-6903-3413-8642-665b0cdce46c>
2012-02-13 11:56:10 +0000

    原来了嫩多文件没上传 坑了1943了。。

---
+++ trunk/nga_phone_base_3.0/src/sp/phone/activity/BookmarkActivity.java
@@ -0,0 +1,153 @@
+package sp.phone.activity;
+
+import java.util.List;
+import com.alibaba.fastjson.JSON;
+
+import sp.phone.bean.Bookmark;
+import sp.phone.forumoperation.FloorOpener;
+import sp.phone.utils.PhoneConfiguration;
+import sp.phone.utils.ReflectionUtil;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.TextView;
+
+public class BookmarkActivity extends Activity 
+	implements OnItemClickListener {
+	List<Bookmark> bookmarks;//reference
+	ListView view;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		 //getLayoutInflater().inflate(R.layout.bookmarks, null);
+		//view.setBackgroundResource(ThemeManager.getInstance().getBackgroundColor());
+		this.setContentView(R.layout.bookmarks);
+		 view = (ListView)findViewById(R.id.bookmark_listview);
+		bookmarks = 
+				PhoneConfiguration.getInstance().getBookmarks();
+
+		
+        view.setAdapter(new BookmarkAdapter());
+        view.setOnItemClickListener(this);
+        //this.getListView();
+        this.registerForContextMenu(view);
+		
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		int flags = ActionBar.DISPLAY_SHOW_HOME;
+		flags |= ActionBar.DISPLAY_USE_LOGO;
+		flags |= ActionBar.DISPLAY_SHOW_TITLE;
+		flags |= ActionBar.DISPLAY_HOME_AS_UP;
+		flags |= ActionBar.DISPLAY_SHOW_CUSTOM;
+		//final ActionBar bar = getActionBar();
+		//bar.setDisplayOptions(flags);
+		ReflectionUtil.actionBar_setDisplayOption(this, flags);
+		return super.onCreateOptionsMenu(menu);
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch( item.getItemId())
+		{
+			
+			case android.R.id.home:
+				this.finish();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		menu.add(0,0,0,"É¾³ý");
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+
+		switch(item.getItemId())
+		{
+			case 0:
+
+				SharedPreferences  share = 
+						getSharedPreferences("perference", MODE_PRIVATE);
+
+					Editor editor = share.edit();
+					bookmarks.remove(info.position);
+					String jsonString = "";
+					if(bookmarks.size()>0)
+						jsonString =JSON.toJSONString(bookmarks.getClass());
+					editor.putString("bookmarks",jsonString  );
+					editor.commit();
+					
+					BookmarkAdapter ad = (BookmarkAdapter) view.getAdapter();
+					ad.notifyDataSetChanged();
+				break;
+			default:
+		}
+		return super.onContextItemSelected(item);
+	}
+	
+	private class BookmarkAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return bookmarks.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			
+			return bookmarks.get(position).getTitle();
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if(convertView ==null){
+				convertView = new TextView(BookmarkActivity.this);
+			}
+			
+			((TextView) convertView).setText(
+					bookmarks.get(position).getTitle());
+			Drawable draw = getResources().getDrawable(android.R.drawable.btn_star_big_on);
+			((TextView) convertView).setCompoundDrawablesWithIntrinsicBounds(draw,null,null,null);
+			return convertView;
+		}
+		
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		new FloorOpener(this).handleFloor(bookmarks.get(position).getUrl());
+		
+	}
+	
+	
+	
+
+}
---
+++ trunk/nga_phone_base_3.0/src/sp/phone/forumoperation/FloorOpener.java
@@ -0,0 +1,89 @@
+package sp.phone.forumoperation;
+
+import java.util.HashMap;
+
+import sp.phone.activity.ArticleListActivity1;
+import sp.phone.activity.MyApp;
+import sp.phone.bean.ArticlePage;
+import sp.phone.utils.HttpUtil;
+import sp.phone.utils.ActivityUtil;
+import android.app.Activity;
+import android.content.Intent;
+
+public 	class FloorOpener {
+	private MyApp app;
+	private Activity activity;
+	HashMap<Object, ArticlePage> map_article;
+	ActivityUtil activityUtil =ActivityUtil.getInstance();
+	public FloorOpener(Activity activity) {
+		super();
+		this.activity = activity;
+		this.app = (MyApp) activity.getApplication();
+		//rssFeed = app.getRssFeed();
+		//map = app.getMap();
+		map_article = app.getMap_article();
+	}
+	public void handleFloor(String floorUrl) {
+		final String url = floorUrl;
+		ArticlePage ap = map_article.get(url + "&page=1");
+		if (ap != null) {
+			app.setArticlePage(ap);
+			Intent intent = new Intent();
+			intent.setClass(activity,
+					ArticleListActivity1.class);
+			activity.startActivity(intent);
+		} else {
+			new Thread() {
+				@Override
+				public void run() {
+					ArticlePage articlePage = null;
+
+					System.out.println("host:" + HttpUtil.HOST);
+					if (!HttpUtil.HOST_PORT.equals("")) {
+						activityUtil.noticeSaying(activity);
+						/*String str = StringUtil.getSaying();
+						if (str.indexOf(";") != -1) {
+							activityUtil.notice("加速模式", str.split(";")[0]
+									+ "-----" + str.split(";")[1]);
+						} else {
+							activityUtil.notice("加速模式", str);
+						}*/
+						articlePage = HttpUtil
+								.getArticlePageByJson(HttpUtil.HOST
+										+ "?uri=" + url + "@page=1");
+					}
+					if (articlePage == null) {
+						// activityUtil.notice("INFO",
+						// "连接策略:P-N,这种策略将模拟浏览器显示方式");
+						activityUtil.noticeSaying(activity);
+						String threadUrl = url;
+						if(!url.startsWith("http://")){
+							threadUrl = "http://bbs.ngacn.cc" + url;
+						}
+						String cookie = "";
+						if (app.getUid() != null && app.getUid() != "")
+							cookie = "ngaPassportUid=" + app.getUid()
+									+ "; ngaPassportCid=" + app.getCid();
+						articlePage = HttpUtil.getArticlePage(threadUrl
+								+ "&page=1", cookie);
+					}
+					if (articlePage != null) {
+						app.setArticlePage(articlePage);// 设置当前page
+						map_article.put(url + "&page=1", articlePage);// 添加新的数据
+						app.setMap_article(map_article);
+						Intent intent = new Intent(activity,
+								ArticleListActivity1.class);
+					
+						activity.startActivity(intent);
+					} else {
+						activityUtil.noticeError( "可能遇到了一个广告或者帖子被删除"
+								,activity);
+					}
+					activityUtil.dismiss();
+				}
+			}.start();
+		}
+	}
+
+}
+
