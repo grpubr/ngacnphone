7b54352d962cbb3932ccb75f4a239428e4af200b
grpubr <grpubr@gmail.com>
2012-02-13 11:56:10 +0000

    原来了嫩多文件没上传 坑了1943了。。

---
+++ nga_phone_base_3.0/src/sp/phone/utils/PhoneConfiguration.java
@@ -0,0 +1,77 @@
+package sp.phone.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import sp.phone.bean.Bookmark;
+
+public class PhoneConfiguration {
+	private static PhoneConfiguration instance;
+	boolean refreshAfterPost;
+	List<Bookmark> bookmarks;// url<-->tilte
+	private float textSize;
+	private int webSize;
+	public float getTextSize() {
+		return textSize;
+	}
+	public void setTextSize(float textSize) {
+		this.textSize = textSize;
+	}
+	public int getWebSize() {
+		return webSize;
+	}
+	public void setWebSize(int webSize) {
+		this.webSize = webSize;
+	}
+	private PhoneConfiguration(){
+
+		bookmarks= new ArrayList<Bookmark>();
+
+		
+	}
+	public boolean isRefreshAfterPost() {
+		return refreshAfterPost;
+	}
+	public void setRefreshAfterPost(boolean refreshAfterPost) {
+		this.refreshAfterPost = refreshAfterPost;
+	}
+	public static PhoneConfiguration getInstance(){
+		if(instance ==null){
+			instance = new PhoneConfiguration();
+		}
+		return instance;
+	}
+	public List<Bookmark> getBookmarks() {
+		return bookmarks;
+	}
+	
+	public void setBookmarks(List<Bookmark> bookmarks) {
+		this.bookmarks = bookmarks;
+	}
+	public boolean addBookmark(String url,String title){
+		boolean ret = true;
+		for(Bookmark b:bookmarks){
+			if(b.getUrl().equals(url))
+				return false;
+			
+		}
+		Bookmark newBookmark = new Bookmark();
+		newBookmark.setTitle(title);
+		newBookmark.setUrl(url);
+		bookmarks.add(newBookmark);
+		return ret;
+	}
+	
+	public boolean removeBookmark(String url){
+
+		for(Bookmark b:bookmarks){
+			if(b.getUrl().equals(url)){
+				bookmarks.remove(b);
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+
+}
---
+++ nga_phone_base_3.0/src/sp/phone/utils/ReflectionUtil.java
@@ -0,0 +1,47 @@
+package sp.phone.utils;
+
+import java.lang.reflect.Method;
+
+import android.app.Activity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+
+public class ReflectionUtil {
+	public static boolean actionBar_setDisplayOption(Activity activity,
+			int flags){
+		boolean ret = true;
+		 Method setDisplayMethod;
+		 Method getActionBarMethod;
+		try {//
+			getActionBarMethod = activity.getClass().
+			 	getMethod("getActionBar");
+			Object actionBar = getActionBarMethod.invoke(activity);
+			
+			//setDisplayMethod= Class.forName("android.app.ActionBar")
+			//		.getMethod("setDisplayOptions", int.class);
+			setDisplayMethod = actionBar.getClass().getMethod("setDisplayOptions", int.class);
+			 setDisplayMethod.invoke(actionBar, flags);
+		} catch (Exception e){
+			Log.i(activity.getClass().getSimpleName(),"fail to set actionBar");
+			ret = false;
+		}
+		return ret;
+		
+	}
+	
+	public static boolean view_setGravity(View v,
+			int flags){
+		boolean ret = true;
+		String methodName = "setGravity";
+		 Method setMethod;
+		try {//
+			setMethod = v.getClass().getMethod(methodName,int.class);
+			 setMethod.invoke(v, flags);
+		} catch (Exception e){
+			Log.i(v.getClass().getSimpleName(),"fail to set gravity");
+		}
+		return ret;
+	}
+
+}
