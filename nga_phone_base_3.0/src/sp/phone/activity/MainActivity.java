3c6e663f9d78fae4a224334f05fcc26c39bb0b17
grpubr <grpubr@gmail.com>
2012-05-22 13:23:46 +0000

    rename activity

--- nga_phone_base_3.0/src/sp/phone/activity/MainActivity.java
+++ nga_phone_base_3.0/src/sp/phone/activity/MainActivity.java
@@ -240,7 +240,7 @@ public class MainActivity extends ActionBarActivity
 		case R.id.mainmenu_exit:
 			Intent intent = new Intent();
 			intent.putExtra("tab", "1");
-			intent.setClass(MainActivity.this, TopicListListActivity.class);
+			intent.setClass(MainActivity.this, TopicListActivity.class);
 			startActivity(intent);
 			overridePendingTransition(R.anim.zoom_enter, R.anim.zoom_exit);
 			break;
