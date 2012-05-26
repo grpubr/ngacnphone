df8273f97fa924096888c51bfd7823c4096d2ad5
grpubr <grpubr@gmail.com>
2012-05-26 15:50:17 +0000

    读取配置的代码放到application里

--- nga_phone_base_3.0/src/sp/phone/activity/MyApp.java
+++ nga_phone_base_3.0/src/sp/phone/activity/MyApp.java
@@ -1,27 +1,134 @@
 package sp.phone.activity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import com.alibaba.fastjson.JSON;
+
 import sp.phone.bean.ArticlePage;
+import sp.phone.bean.Bookmark;
+import sp.phone.bean.PerferenceConstant;
 import sp.phone.bean.RSSFeed;
 import sp.phone.utils.PhoneConfiguration;
+import sp.phone.utils.ThemeManager;
 import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.ActivityInfo;
+import android.util.Log;
 
-public class MyApp extends Application {
-
+public class MyApp extends Application implements PerferenceConstant {
+	final private static String TAG = MyApp.class.getSimpleName();
+	final static int version = 139;
 	private RSSFeed rssFeed;
 	private ArticlePage articlePage;
 	private HashMap<Object, ArticlePage> map_article;
 	private PhoneConfiguration config;
-	
+	boolean newVersion = false;
 	
 	
 	@Override
 	public void onCreate() {
+		Log.d(TAG,"androind start");
 		config = PhoneConfiguration.getInstance();
+		initUserInfo();
+		loadConfig();
 		super.onCreate();
 	}
 
+	private void initUserInfo() {
+		
+		String uid = null;
+		String cid = null;
+		
+		PhoneConfiguration config = PhoneConfiguration.getInstance();
 
+		SharedPreferences share = this.getSharedPreferences(PERFERENCE,
+				MODE_PRIVATE);
+
+		
+			uid = share.getString(UID, "");
+			cid = share.getString(CID, "");
+			if (uid != null && cid != null && uid != "" && cid != "") {
+				config.setUid(uid);
+				config.setCid(cid);
+			}
+			boolean downImgWithoutWifi = share.getBoolean(DOWNLOAD_IMG_NO_WIFI, false);
+			config.setDownImgNoWifi(downImgWithoutWifi);
+			boolean downAvatarNoWifi = share.getBoolean(DOWNLOAD_AVATAR_NO_WIFI, false);
+			config.setDownAvatarNoWifi(downAvatarNoWifi);
+
+	}
+
+	private void loadConfig(){
+		
+		SharedPreferences share = this.getSharedPreferences(PERFERENCE,
+				MODE_PRIVATE);
+		if(share.getBoolean(NIGHT_MODE, false))
+			ThemeManager.getInstance().setMode(1);
+		
+		ThemeManager.getInstance().screenOrentation = 
+				share.getInt(SCREEN_ORENTATION,ActivityInfo.SCREEN_ORIENTATION_USER);
+		
+		
+		int version_in_config = share.getInt(VERSION, 0);
+		if(version_in_config < version){
+			newVersion = true;
+			Editor editor = share.edit();
+			editor.putInt(VERSION, version);
+			editor.putBoolean(REFRESH_AFTER_POST, false);
+
+			editor.commit();
+			
+		}
+		
+
+		//refresh
+		PhoneConfiguration config = PhoneConfiguration.getInstance();
+		config.setRefreshAfterPost(
+				share.getBoolean(REFRESH_AFTER_POST,false));
+		config.setRefreshAfterPost(false);
+		
+		//font
+		final float defTextSize = 21.0f;//new TextView(this).getTextSize();
+		final int defWebSize = 16;//new WebView(this).getSettings().getDefaultFontSize();
+		
+		final float textSize = share.getFloat(TEXT_SIZE, defTextSize);
+		final int webSize = share.getInt(WEB_SIZE, defWebSize);
+		config.setTextSize(textSize);
+		config.setWebSize(webSize);
+		
+		boolean notification = share.getBoolean(ENABLE_NOTIFIACTION, true);
+		boolean notificationSound = share.getBoolean(NOTIFIACTION_SOUND, true);
+		config.notification = notification;
+		config.notificationSound = notificationSound;
+		
+		//bookmarks
+		String bookmarkJson = share.getString(BOOKMARKS, "");
+		List<Bookmark> bookmarks = new ArrayList<Bookmark>();
+		try{
+		if(!bookmarkJson.equals(""))
+			bookmarks=JSON.parseArray(bookmarkJson, Bookmark.class);
+		}catch(Exception e){
+			Log.e("JSON_error",Log.getStackTraceString(e));
+		}
+		PhoneConfiguration.getInstance().setBookmarks(bookmarks);
+		
+		
+		
+	}
+	
+	
+	
+	public boolean isNewVersion() {
+		return newVersion;
+	}
+
+	public void setNewVersion(boolean newVersion) {
+		this.newVersion = newVersion;
+	}
 
 	public HashMap<Object, ArticlePage> getMap_article() {
 		if (map_article == null) {
