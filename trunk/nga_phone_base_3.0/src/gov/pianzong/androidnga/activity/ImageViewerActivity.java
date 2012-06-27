a7fe7067a36cdfc7d6adab0bff1e74eea576d384
grpubr@gmail.com <grpubr@gmail.com@56283f58-6903-3413-8642-665b0cdce46c>
2012-06-27 14:13:42 +0000

    flash还是调用浏览器打开

--- trunk/nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/ImageViewerActivity.java
+++ trunk/nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/ImageViewerActivity.java
@@ -1,46 +1,97 @@
 package gov.pianzong.androidnga.activity;
 
+import sp.phone.utils.ReflectionUtil;
+import sp.phone.utils.ThemeManager;
+import gov.pianzong.androidnga.R;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 
 public class ImageViewerActivity extends FragmentActivity {
-
+	private WebView wv;
 	@Override
 	protected void onCreate(Bundle arg0) {
-		// TODO Auto-generated method stub
-		WebView wv = new WebView(this);
-		this.setContentView(wv);
-		final WebSettings  settings = wv.getSettings(); 
-		settings.setSupportZoom(true);
-		settings.setBuiltInZoomControls(true);
-		//settings.setLayoutAlgorithm(LayoutAlgorithm.NARROW_COLUMNS);
+
+		this.setContentView(R.layout.webview_layout);
+		wv = (WebView) findViewById(R.id.webview);
 		
+		
+		load();
+		
+		super.onCreate(arg0);
+	}
+
+	@Override
+	protected void onResume() {
+		
+		super.onResume();
+	}
+	
+	private void load(){
 		final String uri = getIntent().getStringExtra("path");
+		final WebSettings  settings = wv.getSettings(); 
 		if(uri.endsWith(".swf")
 				&& Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO )//android 2.2
 		{
-			final String html = 
-			"<html><head></head><body><embed src='"
-					+ uri +
-					"' allowFullScreen='true' type='application/x-shockwave-flash'></embed></body></html>";
-			//settings.setJavaScriptEnabled(true);
-			//wv.setWebChromeClient(new WebChromeClient());
+			//final String html = "<embed src='"
+			//		+uri+
+			//	"' allowFullScreen='true' quality='high'  align='middle' allowScriptAccess='always' type='application/x-shockwave-flash'></embed>";
+			settings.setJavaScriptEnabled(true);
+			wv.setWebChromeClient(new WebChromeClient());
 			settings.setPluginState(PluginState.ON);
-			wv.loadData(html, "text/html", "utf-8");
-		}else{
+			wv.loadUrl(uri);
+
+		}else{//images
+
+			settings.setSupportZoom(true);
+			settings.setBuiltInZoomControls(true);	
 			settings.setUseWideViewPort(true); 
 			settings.setLoadWithOverviewMode(true);
 			wv.loadUrl(uri);
 		}
 		
-		
-		super.onCreate(arg0);
 	}
+	
+	
+
+	@Override
+	protected void onPause() {
+		wv.loadUrl("about:blank");
+		super.onPause();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		this.getMenuInflater().inflate(R.menu.imageview_option_menu, menu);
+		final int flags = ThemeManager.ACTION_BAR_FLAG;
+		ReflectionUtil.actionBar_setDisplayOption(this, flags);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.item_refresh :
+			load();
+			break;
+		default:
+			Intent MyIntent = new Intent(Intent.ACTION_MAIN);
+			MyIntent.setClass(this, ArticleListActivity.class);
+			MyIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(MyIntent);
+				
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
 
 	
 }
