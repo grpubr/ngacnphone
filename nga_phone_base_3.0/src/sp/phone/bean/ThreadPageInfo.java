595586c10acf7a288a72b62f8b9618b1b5cf8507
grpubr <grpubr@gmail.com>
2012-07-25 10:06:51 +0000

    json 只解析需要的字段

--- nga_phone_base_3.0/src/sp/phone/bean/ThreadPageInfo.java
+++ nga_phone_base_3.0/src/sp/phone/bean/ThreadPageInfo.java
@@ -5,83 +5,38 @@ public class ThreadPageInfo {
 	private int tid;
 	private int fid;
 	private int quote_from;
-	private String quote_to;
-	private String icon;
+	//private String quote_to;
+	//private String icon;
 	private String titlefont;
 	private String author;
-	private int authorid;
+	//private int authorid;
 	private String subject;
-	private int ifmark;
-	private int type;
-	private int type_2;
-	//private int postdate;
-	private String postdate;
-	private int lastpost;
+	//private int ifmark;
+	//private int type;
+	//private int type_2;
+	//private String postdate;
+	//private int lastpost;
 	private String lastposter;
 	private int replies;
-	private int locked;
-	private int digest;
-	private int ifupload;
-	private int lastmodify;
-	private int recommend;
-	private int view_count;
-	private String fid_no_match;
-	private String subject_org;
-	//private int postdate_org;
-	//private int lastpost_org;
+	//private int locked;
+	//private int digest;
+	//private int ifupload;
+	//private int lastmodify;
+	//private int recommend;
+	//private int view_count;
+	//private String fid_no_match;
+	//private String subject_org;
     private String lastposter_org;
-    private String tpcurl;
-    private String ispage;
+    //private String tpcurl;
+    //private String ispage;
     private int pid;
-    private String adminbox;
+   // private String adminbox;
     private String content;
-	public int getTid() {
-		return tid;
-	}
-	public void setTid(int tid) {
-		this.tid = tid;
-	}
 	public int getFid() {
 		return fid;
 	}
 	public void setFid(int fid) {
 		this.fid = fid;
-	}
-	public int getQuote_from() {
-		return quote_from;
-	}
-	public void setQuote_from(int quote_from) {
-		this.quote_from = quote_from;
-	}
-	public String getQuote_to() {
-		return quote_to;
-	}
-	public void setQuote_to(String quote_to) {
-		this.quote_to = quote_to;
-	}
-	public String getIcon() {
-		return icon;
-	}
-	public void setIcon(String icon) {
-		this.icon = icon;
-	}
-	public String getTitlefont() {
-		return titlefont;
-	}
-	public void setTitlefont(String titlefont) {
-		this.titlefont = titlefont;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public int getAuthorid() {
-		return authorid;
-	}
-	public void setAuthorid(int authorid) {
-		this.authorid = authorid;
 	}
 	public String getSubject() {
 		return subject;
@@ -89,113 +44,23 @@ public class ThreadPageInfo {
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
-	public int getIfmark() {
-		return ifmark;
-	}
-	public void setIfmark(int ifmark) {
-		this.ifmark = ifmark;
-	}
-	public int getType() {
-		return type;
-	}
-	public void setType(int type) {
-		this.type = type;
-	}
-	public int getType_2() {
-		return type_2;
-	}
-	public void setType_2(int type_2) {
-		this.type_2 = type_2;
-	}
-	public String getPostdate() {
-		return postdate;
-	}
-	public void setPostdate(String postdate) {
-		this.postdate = postdate;
-	}
-	public int getLastpost() {
-		return lastpost;
-	}
-	public void setLastpost(int lastpost) {
-		this.lastpost = lastpost;
-	}
-	public String getLastposter() {
-		return lastposter;
-	}
-	public void setLastposter(String lastposter) {
-		this.lastposter = lastposter;
-	}
 	public int getReplies() {
 		return replies;
 	}
 	public void setReplies(int replies) {
 		this.replies = replies;
 	}
-	public int getLocked() {
-		return locked;
+	public int getQuote_from() {
+		return quote_from;
 	}
-	public void setLocked(int locked) {
-		this.locked = locked;
+	public void setQuote_from(int quote_from) {
+		this.quote_from = quote_from;
 	}
-	public int getDigest() {
-		return digest;
+	public int getTid() {
+		return tid;
 	}
-	public void setDigest(int digest) {
-		this.digest = digest;
-	}
-	public int getIfupload() {
-		return ifupload;
-	}
-	public void setIfupload(int ifupload) {
-		this.ifupload = ifupload;
-	}
-	public int getLastmodify() {
-		return lastmodify;
-	}
-	public void setLastmodify(int lastmodify) {
-		this.lastmodify = lastmodify;
-	}
-	public int getRecommend() {
-		return recommend;
-	}
-	public void setRecommend(int recommend) {
-		this.recommend = recommend;
-	}
-	public int getView_count() {
-		return view_count;
-	}
-	public void setView_count(int view_count) {
-		this.view_count = view_count;
-	}
-	public String getFid_no_match() {
-		return fid_no_match;
-	}
-	public void setFid_no_match(String fid_no_match) {
-		this.fid_no_match = fid_no_match;
-	}
-	public String getSubject_org() {
-		return subject_org;
-	}
-	public void setSubject_org(String subject_org) {
-		this.subject_org = subject_org;
-	}
-	public String getLastposter_org() {
-		return lastposter_org;
-	}
-	public void setLastposter_org(String lastposter_org) {
-		this.lastposter_org = lastposter_org;
-	}
-	public String getTpcurl() {
-		return tpcurl;
-	}
-	public void setTpcurl(String tpcurl) {
-		this.tpcurl = tpcurl;
-	}
-	public String getIspage() {
-		return ispage;
-	}
-	public void setIspage(String ispage) {
-		this.ispage = ispage;
+	public void setTid(int tid) {
+		this.tid = tid;
 	}
 	public int getPid() {
 		return pid;
@@ -203,11 +68,29 @@ public class ThreadPageInfo {
 	public void setPid(int pid) {
 		this.pid = pid;
 	}
-	public String getAdminbox() {
-		return adminbox;
+	public String getAuthor() {
+		return author;
 	}
-	public void setAdminbox(String adminbox) {
-		this.adminbox = adminbox;
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getLastposter() {
+		return lastposter;
+	}
+	public void setLastposter(String lastposter) {
+		this.lastposter = lastposter;
+	}
+	public String getLastposter_org() {
+		return lastposter_org;
+	}
+	public void setLastposter_org(String lastposter_org) {
+		this.lastposter_org = lastposter_org;
+	}
+	public String getTitlefont() {
+		return titlefont;
+	}
+	public void setTitlefont(String titlefont) {
+		this.titlefont = titlefont;
 	}
 	public String getContent() {
 		return content;
@@ -215,6 +98,7 @@ public class ThreadPageInfo {
 	public void setContent(String content) {
 		this.content = content;
 	}
+	
 	
 	
 
--- nga_phone_base_3.0/src/sp/phone/bean/ThreadRowInfo.java
+++ nga_phone_base_3.0/src/sp/phone/bean/ThreadRowInfo.java
@@ -5,311 +5,61 @@ import java.util.Map;
 
 public class ThreadRowInfo {
 	
-	private int tid;
-	private int fid;
-	 private int quote_from;
-	 private String quote_to;
-	 private String icon;
-	 private String titlefont;
+	//private int tid;
+	//private String fid;
+	// private int quote_from;
+	 //private String quote_to;
+	// private String icon;
+	// private String titlefont;
 	 private String author;
 	 private int authorid;
 	 private String subject;
-	 private int type;
-	 private int type_2;
+	// private int type;
+	// private int type_2;
 	private String postdate;
-	 private int lastpost;
-	 private String lastposter;
-	private int replies;
-	 private int locked;
-	 private int digest;
-	 private int ifupload;
-	 private int lastmodify;
-	 private int recommend;
+	// private int lastpost;
+	// private String lastposter;
+	//private int replies;
+	// private int locked;
+	// private int digest;
+	// private int ifupload;
+	// private int lastmodify;
+	// private int recommend;
 	private int pid;
 	private String alterinfo;
 	private String content;
 	private int lou;
-	private int postdatetimestamp;
-	private int content_length;
+	//private int postdatetimestamp;
+	//private int content_length;
 	private Map<String,Attachment>attachs;
-	private int credit;
-	private String reputation;
-	private int groupid;
-	private String lpic;
-	private String level;
-	private int gp_lesser;
-	private int yz;
-	private String js_escap_site;
-	private String js_escap_honor;
+	//private int credit;
+	//private String reputation;
+	//private int groupid;
+	//private String lpic;
+	//private String level;
+	//private int gp_lesser;
+	//private int yz;
+	//private String js_escap_site;
+	//private String js_escap_honor;
 	private String js_escap_avatar;
-	private int regdate;
-	private String mute_time;
-	private int postnum;
-	private int aurvrc;
-	private int money;
-	private int thisvisit;
+	//private int regdate;
+	//private String mute_time;
+	//private int postnum;
+	//private int aurvrc;
+	//private int money;
+	//private int thisvisit;
 	private String signature;
-	private String nickname;
+	//private String nickname;
 	private List<ThreadRowInfo> comments;
 	
-	public int getTid() {
-		return tid;
-	}
-	public void setTid(int tid) {
-		this.tid = tid;
-	}
-	public int getFid() {
-		return fid;
-	}
-	public void setFid(int fid) {
-		this.fid = fid;
-	}
-	public int getQuote_from() {
-		return quote_from;
-	}
-	public void setQuote_from(int quote_from) {
-		this.quote_from = quote_from;
-	}
-	public String getQuote_to() {
-		return quote_to;
-	}
-	public void setQuote_to(String quote_to) {
-		this.quote_to = quote_to;
-	}
 	
-	public String getIcon() {
-		return icon;
-	}
-	public void setIcon(String icon) {
-		this.icon = icon;
-	}
-	public String getTitlefont() {
-		return titlefont;
-	}
-	public void setTitlefont(String titlefont) {
-		this.titlefont = titlefont;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	public int getAuthorid() {
-		return authorid;
-	}
-	public void setAuthorid(int authorid) {
-		this.authorid = authorid;
-	}
-	public String getSubject() {
-		return subject;
-	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	public int getType() {
-		return type;
-	}
-	public void setType(int type) {
-		this.type = type;
-	}
-	public int getType_2() {
-		return type_2;
-	}
-	public void setType_2(int type_2) {
-		this.type_2 = type_2;
-	}
-	public String getPostdate() {
-		return postdate;
-	}
-	public void setPostdate(String postdate) {
-		this.postdate = postdate;
-	}
-	public int getLastpost() {
-		return lastpost;
-	}
-	public void setLastpost(int lastpost) {
-		this.lastpost = lastpost;
-	}
-	public String getLastposter() {
-		return lastposter;
-	}
-	public void setLastposter(String lastposter) {
-		this.lastposter = lastposter;
-	}
-	public int getReplies() {
-		return replies;
-	}
-	public void setReplies(int replies) {
-		this.replies = replies;
-	}
-	public int getLocked() {
-		return locked;
-	}
-	public void setLocked(int locked) {
-		this.locked = locked;
-	}
-	public int getDigest() {
-		return digest;
-	}
-	public void setDigest(int digest) {
-		this.digest = digest;
-	}
-	public int getIfupload() {
-		return ifupload;
-	}
-	public void setIfupload(int ifupload) {
-		this.ifupload = ifupload;
-	}
-	public int getLastmodify() {
-		return lastmodify;
-	}
-	public void setLastmodify(int lastmodify) {
-		this.lastmodify = lastmodify;
-	}
-	public int getRecommend() {
-		return recommend;
-	}
-	public void setRecommend(int recommend) {
-		this.recommend = recommend;
-	}
-	public int getPid() {
-		return pid;
-	}
-	public void setPid(int pid) {
-		this.pid = pid;
-	}
-	public String getAlterinfo() {
-		return alterinfo;
-	}
-	public void setAlterinfo(String alterinfo) {
-		this.alterinfo = alterinfo;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public int getLou() {
-		return lou;
-	}
-	public void setLou(int lou) {
-		this.lou = lou;
-	}
-	public int getPostdatetimestamp() {
-		return postdatetimestamp;
-	}
-	public void setPostdatetimestamp(int postdatetimestamp) {
-		this.postdatetimestamp = postdatetimestamp;
-	}
-	public int getContent_length() {
-		return content_length;
-	}
-	public void setContent_length(int content_length) {
-		this.content_length = content_length;
-	}
-	public Map<String,Attachment> getAttachs() {
+	
+	
+	public Map<String, Attachment> getAttachs() {
 		return attachs;
 	}
-	public void setAttachs(Map<String,Attachment> attachs) {
+	public void setAttachs(Map<String, Attachment> attachs) {
 		this.attachs = attachs;
-	}
-	public int getCredit() {
-		return credit;
-	}
-	public void setCredit(int credit) {
-		this.credit = credit;
-	}
-	public String getReputation() {
-		return reputation;
-	}
-	public void setReputation(String reputation) {
-		this.reputation = reputation;
-	}
-	public int getGroupid() {
-		return groupid;
-	}
-	public void setGroupid(int groupid) {
-		this.groupid = groupid;
-	}
-	public String getLpic() {
-		return lpic;
-	}
-	public void setLpic(String lpic) {
-		this.lpic = lpic;
-	}
-	public String getLevel() {
-		return level;
-	}
-	public void setLevel(String level) {
-		this.level = level;
-	}
-	public int getGp_lesser() {
-		return gp_lesser;
-	}
-	public void setGp_lesser(int gp_lesser) {
-		this.gp_lesser = gp_lesser;
-	}
-	public int getYz() {
-		return yz;
-	}
-	public void setYz(int yz) {
-		this.yz = yz;
-	}
-	public String getJs_escap_site() {
-		return js_escap_site;
-	}
-	public void setJs_escap_site(String js_escap_site) {
-		this.js_escap_site = js_escap_site;
-	}
-	public String getJs_escap_honor() {
-		return js_escap_honor;
-	}
-	public void setJs_escap_honor(String js_escap_honor) {
-		this.js_escap_honor = js_escap_honor;
-	}
-	public String getJs_escap_avatar() {
-		return js_escap_avatar;
-	}
-	public void setJs_escap_avatar(String js_escap_avatar) {
-		this.js_escap_avatar = js_escap_avatar;
-	}
-	public int getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(int regdate) {
-		this.regdate = regdate;
-	}
-	public String getMute_time() {
-		return mute_time;
-	}
-	public void setMute_time(String mute_time) {
-		this.mute_time = mute_time;
-	}
-	public int getPostnum() {
-		return postnum;
-	}
-	public void setPostnum(int postnum) {
-		this.postnum = postnum;
-	}
-	public int getAurvrc() {
-		return aurvrc;
-	}
-	public void setAurvrc(int aurvrc) {
-		this.aurvrc = aurvrc;
-	}
-	public int getMoney() {
-		return money;
-	}
-	public void setMoney(int money) {
-		this.money = money;
-	}
-	public int getThisvisit() {
-		return thisvisit;
-	}
-	public void setThisvisit(int thisvisit) {
-		this.thisvisit = thisvisit;
 	}
 	public String getSignature() {
 		return signature;
@@ -317,11 +67,59 @@ public class ThreadRowInfo {
 	public void setSignature(String signature) {
 		this.signature = signature;
 	}
-	public String getNickname() {
-		return nickname;
+	public String getJs_escap_avatar() {
+		return js_escap_avatar;
 	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
+	public void setJs_escap_avatar(String js_escap_avatar) {
+		this.js_escap_avatar = js_escap_avatar;
+	}
+	public String getAlterinfo() {
+		return alterinfo;
+	}
+	public void setAlterinfo(String alterinfo) {
+		this.alterinfo = alterinfo;
+	}
+	public int getLou() {
+		return lou;
+	}
+	public void setLou(int lou) {
+		this.lou = lou;
+	}
+	public int getAuthorid() {
+		return authorid;
+	}
+	public void setAuthorid(int authorid) {
+		this.authorid = authorid;
+	}
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getPostdate() {
+		return postdate;
+	}
+	public void setPostdate(String postdate) {
+		this.postdate = postdate;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
 	}
 	public List<ThreadRowInfo> getComments() {
 		return comments;
