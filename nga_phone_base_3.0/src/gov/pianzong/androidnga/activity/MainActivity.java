7674c8691a2f9f18f914e6a0ecb8fc2eb17ee96f
grpubr <grpubr@gmail.com>
2012-08-03 12:39:55 +0000

    3个新板块

--- nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/MainActivity.java
+++ nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/MainActivity.java
@@ -400,9 +400,12 @@ public class MainActivity extends ActionBarActivity
 		boards.add(new Board(i, "-608808", " 弑熊主厨的血腥厨房", R.drawable.pdefault));
 		boards.add(new Board(i, "-469608", " 影视讨论", R.drawable.pdefault));
 		boards.add(new Board(i, "-55912", "音乐讨论", R.drawable.pdefault));
-		boards.add(new Board(i, "-60341", "宠物嘉年华", R.drawable.pdefault));
+		boards.add(new Board(i, "-353371", "傻乎乎的小宠物", R.drawable.pdefault));
 		boards.add(new Board(i, "-538800", "乙女向二次元", R.drawable.pdefault));
 		boards.add(new Board(i, "-522679", "Battlefield 3讨论版", R.drawable.pdefault));
+		boards.add(new Board(i, "-7678526", "艾泽拉斯麻将科学院", R.drawable.pdefault));
+		boards.add(new Board(i, "-202020", " 一只IT喵的自我修养", R.drawable.pdefault));
+		boards.add(new Board(i, "-444012", "自行车：我们的骑迹", R.drawable.pdefault));
 		boards.addCategoryName(i, "个人版面");
 		i++;
 		
