690bdfc05303ed5472c2e18d760bfda0c4ea00c0
grpubr <grpubr@gmail.com>
2012-08-08 13:22:08 +0000

    bug修复

--- nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/MyApp.java
+++ nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/MyApp.java
@@ -19,7 +19,7 @@ import com.alibaba.fastjson.JSON;
 
 public class MyApp extends Application implements PerferenceConstant {
 	final private static String TAG = MyApp.class.getSimpleName();
-	public final static int version = 330;
+	public final static int version = 331;
 	private PhoneConfiguration config = null;
 	boolean newVersion = false;
 	
