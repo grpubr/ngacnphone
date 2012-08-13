b84f6b17f7e8f3d1f64aeefa6ad79e631c3313b4
grpubr <grpubr@gmail.com>
2012-08-13 11:43:03 +0000

    键盘遮住发帖按钮的问题

--- nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/MyApp.java
+++ nga_phone_base_3.0/src/gov/pianzong/androidnga/activity/MyApp.java
@@ -19,7 +19,7 @@ import com.alibaba.fastjson.JSON;
 
 public class MyApp extends Application implements PerferenceConstant {
 	final private static String TAG = MyApp.class.getSimpleName();
-	public final static int version = 333;
+	public final static int version = 335;
 	private PhoneConfiguration config = null;
 	boolean newVersion = false;
 	
