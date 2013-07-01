f1bfff2bc2aad34fd61103b96dc7d5106875710b
grpubr@gmail.com <grpubr@gmail.com@56283f58-6903-3413-8642-665b0cdce46c>
2013-07-01 01:55:39 +0000

    for cnbeta client

---
+++ trunk/HoloCnbeta/HoloCnbeta/src/main/java/gov/pianzong/interfaces/NewsClickedListener.java
@@ -0,0 +1,10 @@
+package gov.pianzong.interfaces;
+
+import gov.pianzong.bean.NewsInfo;
+
+/**
+ * Created by Administrator on 13-7-1.
+ */
+public interface NewsClickedListener {
+    public  void onClick(NewsInfo info);
+}
