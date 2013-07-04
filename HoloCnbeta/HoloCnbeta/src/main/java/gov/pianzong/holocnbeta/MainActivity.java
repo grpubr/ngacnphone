6f17779418d71bf8e298a38392d1e37cacd250a1
grpubr <grpubr@gmail.com>
2013-07-04 13:36:38 +0000

    改了下缓存大小和位置

--- HoloCnbeta/HoloCnbeta/src/main/java/gov/pianzong/holocnbeta/MainActivity.java
+++ HoloCnbeta/HoloCnbeta/src/main/java/gov/pianzong/holocnbeta/MainActivity.java
@@ -4,23 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.http.HttpResponseCache;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ListView;
 
 import java.io.File;
 import java.io.IOException;
 
 import gov.pianzong.adapter.HeadlinePagerAdapter;
-import gov.pianzong.adapter.NewsListAdapter;
 import gov.pianzong.bean.NewsInfo;
 import gov.pianzong.interfaces.NewsClickedListener;
-import gov.pianzong.task.NewsListLoadTask;
 import gov.pianzong.util.AppConstants;
-import gov.pianzong.util.StringUtil;
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher;
 
 public class MainActivity extends Activity implements NewsClickedListener {
@@ -41,11 +36,9 @@ public class MainActivity extends Activity implements NewsClickedListener {
 
     private  void installHtppCache(){
         try {
-            File dir = getExternalCacheDir();
-            if(dir == null)
-                dir = getCacheDir();
+            File dir = getCacheDir();
             File httpCacheDir = new File( dir, "logoCache");
-            long httpCacheSize = 50 * 1024 * 1024; // 50 MiB
+            long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
              HttpResponseCache.install(httpCacheDir, httpCacheSize);
         }catch (IOException e) {
                 Log.i(getClass().getSimpleName(), "HTTP response cache installation failed:" + e);
