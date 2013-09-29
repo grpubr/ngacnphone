597f30c34d584b74a3d0b61b527383b104fba381
grpubr <grpubr@gmail.com>
2013-09-29 12:13:32 +0000

    swipe back library

---
+++ SwipeBack/src/main/gen/me/imid/swipebacklayout/lib/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package me.imid.swipebacklayout.lib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
